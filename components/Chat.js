ae30fecd6d3fbb59fa63da60cfb00b440e2ea449
priegels <88594441+priegels@users.noreply.github.com>
2022-01-31 20:55:43 +0100

    Basic setup of second screen

---
+++ components/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Chat extends React.Component {
+  render() {
+    //entered name state from Start screen gets displayed in status bar at the top of the app
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name});
+
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello Chat!</Text>
+      </View>
+    )
+  }
+}
