ff581cf4ef7da9acb1d3753ea06c925962126df3
priegels <88594441+priegels@users.noreply.github.com>
2022-01-31 20:55:32 +0100

    Initialized main file + created Stack Navigator

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+// importing screens we want to navigate between
+import Start from './components/Start';
+import Chat from './components/Chat';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+
+// create navigator
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+        initialRouteName= "Start">
+          <Stack.Screen
+            name="Start"
+            component={Start} />
+          <Stack.Screen
+            name="Chat"
+            component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
+// style sheet at the bottom
 const styles = StyleSheet.create({
   container: {
     flex: 1,
