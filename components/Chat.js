1586a529eb25f14c16d2d3dfb635ad880014b40b
priegels <88594441+priegels@users.noreply.github.com>
2022-02-02 08:26:58 +0100

    installed gifted chat + implemented chat bubble + system message

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,71 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+
+import { GiftedChat, Bubble, Day} from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+/* componentDidMount function gets called after Chat component mounts. State gets set
+with static message so you see each element of the UI displayed on screen with setState function */
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {},
+          right: {},
+        }}
+      />
+    )
+  };
+
+  renderDay(props) {
+    const { bgColor } = this.props.route.params;
+    return (
+      <Day
+        {...props}
+        textStyle= {{color: bgColor === '#B9C6AE' ? '#555555' : '#dddddd'}}
+      />
+    );
+  }
+
   render() {
     //entered name state from Start screen gets displayed in status bar at the top of the app
     let name = this.props.route.params.name;
@@ -13,10 +77,19 @@ export default class Chat extends React.Component {
       <View style={{
         flex: 1, 
         justifyContent: 'center', 
-        alignItems: 'center',
         backgroundColor: bgColor
         }}>
-        <Text>Hello Chat!</Text>
+          <GiftedChat 
+            messages={this.state.messages}
+            onSend={(messages) => this.onSend(messages)}
+            renderBubble={this.renderBubble.bind(this)}
+            renderDay={this.renderDay.bind(this)}
+            user={{
+              _id: 1,
+            }}
+          />
+
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
       </View>
     )
   }
